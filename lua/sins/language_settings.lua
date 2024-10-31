291d0c2e36cde2a512e35cee542f38d5b2a569b8
bradlycarpenter <bradlycarpenterza@gmail.com>
2024-10-31 23:10:19 +0200

    update/gdconfig

--- lua/sins/language_settings.lua
+++ lua/sins/language_settings.lua
@@ -48,6 +48,6 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
-    vim.opt_local.expandtab = true
+    vim.opt_local.expandtab = false
   end,
 })
